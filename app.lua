fb90731d1153ca5b2e6b9f3821692dba0ae9afa6
ropoko <rodrigostramantinoli@gmail.com>
2025-12-06 13:16:46 -0300

    feat: default pages

--- app.lua
+++ app.lua
@@ -1,8 +1,20 @@
 local lapis = require("lapis")
+
 local app = lapis.Application()
 
-app:get("/", function()
-  return "Welcome to Lapis " .. require("lapis.version")
+app:enable("etlua")
+app.layout = require "views.layout"
+
+app:get("/", function(self)
+	return { render = "index" }
+end)
+
+app:get("/signup", function(self)
+	return { render = "signup" }
+end)
+
+app:get("/signin", function(self)
+	return { render = "signin" }
 end)
 
 return app
--- template-0.0.0-1.rockspec
+++ template-0.0.0-1.rockspec
@@ -1,4 +1,4 @@
-package = "wildleague"
+package = "template"
 version = "0.0.1-1"
 
 source = {
@@ -12,6 +12,7 @@ description = {
 }
 
 dependencies = {
+
 	"lua-resty-jwt = 0.2.3-0",
 	"lapis = 1.14.0-1",
   "bcrypt = 2.3-1"
